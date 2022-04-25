e8334adfd08f187cca8de3c4b48e3cca81f60ab1
Dmitry Glinyanov <gliv321@gmail.com>
2022-04-25 16:48:54 +0700

    Added dateUpdated to API output.

--- oracle/schemas.py
+++ oracle/schemas.py
@@ -1,4 +1,4 @@
-from typing import List, Literal, Optional
+from typing import List, Literal, Optional, Union
 
 from pydantic import BaseModel, Field
 
@@ -10,6 +10,7 @@ class PairSchemaOut(BaseModel):
     token_1: Optional[str] = Field(alias='token1')
     token_0_supply: str = Field(alias='token0Supply')
     token_1_supply: str = Field(alias='token1Supply')
+    date_updated: Optional[str] = Field(alias='dateUpdated')
 
     class Config:
         allow_population_by_field_name = True
@@ -27,18 +28,23 @@ class BscSchemaOut(PairSchemaOut):
     protocol_name: Literal['pancakeswap'] = Field(default='pancakeswap', alias='protocolName')
 
 
-class PoolsSchamaOut(BaseModel):
+class PoolsSchemaOut(BaseModel):
     ethereum: List[EthereumSchemaOut]
     polygon: List[PolygonSchemaOut]
     bsc: List[BscSchemaOut]
 
 
+class ChainGasSchemaOut(BaseModel):
+    value: Optional[float]
+    date_updated: Optional[str] = Field(alias='dateUpdated')
+
+
 class GasSchemaOut(BaseModel):
-    ethereum: Optional[float]
-    polygon: Optional[float]
-    bsc: Optional[float]
+    ethereum: Union[ChainGasSchemaOut, float, None]
+    polygon: Union[ChainGasSchemaOut, float, None]
+    bsc: Union[ChainGasSchemaOut, float, None]
 
 
 class OracleSchemaOut(BaseModel):
-    pools: Optional[PoolsSchamaOut]
+    pools: Optional[PoolsSchemaOut]
     gas: Optional[GasSchemaOut]
